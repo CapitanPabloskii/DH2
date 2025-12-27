7e4bb6f1e01312d4efea3fa37ca3beb07b6e779a
CapitanPabloskii <choppa555@yahoo.com>
2025-12-27 18:47:41 +0100

    Update pokedex.ts

--- data/mods/placeholder/pokedex.ts
+++ data/mods/placeholder/pokedex.ts
@@ -1,3 +1,10 @@
 export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+shovlfae: {
+		num: -0329478,
+		name: "Shovlfae",
+		types: ["Ground", "Fairy"],
+		baseStats: {hp: 100, atk: 115, def: 120, spa: 52, spd: 90, spe: 58},
+		abilities: {0: "Earth Eater", 1: "Effect Spore", H: "Steel Worker"},
+	},
 
 };
